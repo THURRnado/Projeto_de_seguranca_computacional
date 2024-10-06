8c70a2addf1f0128c7b7fa874beac4d8b0301384
Kalil Teotonio <80278976+Galill@users.noreply.github.com>
2024-10-06 17:43:56 -0300

    Update Main.cpp

--- HashCracker/Main.cpp
+++ HashCracker/Main.cpp
@@ -1,121 +1,70 @@
 #include <iostream>
 #include "Headers.h"
 
-#define TESTE "realhuman_phill.txt"
+#define TESTE "D:\\Dev\\realhuman_phill_sem_diabo.txt"
 
-std::string DescobreHash(std::string s) {
-
-    std::string hash_type;
-
-    switch (s.length()) {
-    
-    case 32:
-        hash_type = "MD5";
-        break;
-
-    case 40:
-        hash_type = "SHA-1";
-        break;
-
-    case 64:
-        hash_type = "SHA-256";
-        break;
-
-    default:
-        hash_type = "ERROR";
-        break;
-    }
-
-    return hash_type;
-}
-
-std::string md5(const std::string& str) {
-    unsigned char hash[MD5_DIGEST_LENGTH];
-
-    MD5_CTX md5;
-    MD5_Init(&md5);
-    MD5_Update(&md5, str.c_str(), str.size());
-    MD5_Final(hash, &md5);
-
-    std::stringstream ss;
-
-    for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
-        ss << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(hash[i]);
-    }
-    return ss.str();
-}
-
-std::string sha1(const std::string& str) {
-    unsigned char hash[SHA_DIGEST_LENGTH];
-
-    SHA_CTX sha1;
-    SHA1_Init(&sha1);
-    SHA1_Update(&sha1, str.c_str(), str.size());
-    SHA1_Final(hash, &sha1);
-
-    std::stringstream ss;
-
-    for (int i = 0; i < SHA_DIGEST_LENGTH; i++) {
-        ss << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(hash[i]);
-    }
-    return ss.str();
-}
-
-std::string sha256(const std::string& str) {
-    unsigned char hash[SHA256_DIGEST_LENGTH];
-
-    SHA256_CTX sha256;
-    SHA256_Init(&sha256);
-    SHA256_Update(&sha256, str.c_str(), str.size());
-    SHA256_Final(hash, &sha256);
-
-    std::stringstream ss;
-
-    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
-        ss << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(hash[i]);
-    }
-    return ss.str();
-}
-
-std::string QuebraHash(std::string hash, std::string hash_type) {
-    int x = 1;
+void CarregaDicionario(std::vector<std::string>& dictionary, std::string dictionary_path) {
+    std::string line;
     std::ifstream in_file;
-    std::string password, hashed_password;
-    in_file.open(TESTE);
+
+    in_file.open(dictionary_path);
 
     if (!in_file.is_open()) {
-        std::cout << "Erro ao abrir o arquivo!" << std::endl;
+        std::cout << "Nao foi possivel abrir o arquivo." << std::endl;
         exit(1);
     }
 
+    while (!in_file.eof()) {
+        std::getline(in_file, line);
+        dictionary.push_back(line);
+    }
+    
+    in_file.close();
+
+}
+
+std::string QuebraHash(std::vector<std::string> dictionary, std::string hash, std::string hash_type) {
+    std::string password, hashed_password;
+
     if (hash_type.compare("MD5") == 0) {
-        while (true) {
-            std::getline(in_file, password);
+        for (int i = 0; i < dictionary.size(); ++i) {
+            password = dictionary[i];
             hashed_password = md5(password);
-            std::cout << x++ << std::endl;
             if (hashed_password.compare(hash) == 0) {
+                std::cout << password << std::endl;
                 break;
             }
         }
     }
 
     if (hash_type.compare("SHA-1") == 0) {
-        while (true) {
-            std::getline(in_file, password);
+        for (int i = 0; i < dictionary.size(); ++i) {
+            password = dictionary[i];
             hashed_password = sha1(password);
-            std::cout << x++ << std::endl;
             if (hashed_password.compare(hash) == 0) {
+                std::cout << password << std::endl;
                 break;
             }
         }
     }
 
     if (hash_type.compare("SHA-256") == 0) {
-        while (true) {
-            std::getline(in_file, password);
+        for (int i = 0; i < dictionary.size(); ++i) {
+            password = dictionary[i];
             hashed_password = sha256(password);
-            std::cout << x++ << std::endl;
             if (hashed_password.compare(hash) == 0) {
+                std::cout << password << std::endl;
+                break;
+            }
+        }
+    }
+
+    if (hash_type.compare("SHA-512") == 0) {
+        for (int i = 0; i < dictionary.size(); ++i) {
+            password = dictionary[i];
+            hashed_password = sha512(password);
+            if (hashed_password.compare(hash) == 0) {
+                std::cout << password << std::endl;
                 break;
             }
         }
@@ -125,24 +74,40 @@ std::string QuebraHash(std::string hash, std::string hash_type) {
 }
 
 int main(int argc, char** argv) {
-    std::string hash, hash_type;
+    int thread_count;
+    std::string hash, hash_type, dictionary_path = TESTE;
+    std::vector<std::string> dictionary;
 
-    std::cout << "Insira o hash: ";
-    std::cin >> hash;
+    std::cout << "Carregando dicionario... " << std::endl;
+    CarregaDicionario(dictionary, dictionary_path);
+    std::cout << "Dicionario carregado.\n " << std::endl;
+    std::cout << "Insira o numero de threads: ";
+    std::cin >> thread_count;
 
-    hash_type = DescobreHash(hash);
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
-    auto start = std::chrono::steady_clock::now();
+    while(true){ 
+        std::cout << "Insira o hash: ";
+        std::getline(std::cin, hash);
 
-    std::string resultado = QuebraHash(hash, hash_type);
+        if (hash.empty()) {
+            std::cout << "\nPrograma encerrado.\n" << std::endl;
+            break;
+        }
 
-    auto end = std::chrono::steady_clock::now();
+        hash_type = DescobreHash(hash);
 
-    std::chrono::duration<double> elapsed = end - start;
+        auto start = std::chrono::high_resolution_clock::now();
 
-    std::cout << "Tipo de hash: " << hash_type << std::endl;
-    std::cout << "Senha encontrada: " << resultado << std::endl;
-    std::cout << "Tempo para quebrar o hash: " << elapsed.count() << " segundos" << std::endl;
+        Tomie(std::ref(dictionary), hash, hash_type, thread_count);
+
+        auto end = std::chrono::high_resolution_clock::now();
+
+        std::chrono::duration<double> elapsed = end - start;
+
+        std::cout << "Tipo de hash: " << hash_type << std::endl;
+        std::cout << "Tempo para quebrar o hash: " << elapsed.count() << " segundos" << std::endl;
+    }
 
     return 0;
 }
