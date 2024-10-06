3abf172aa8baac0c43f9994d2e8c31a26c1121ba
Kalil Teotonio <80278976+Galill@users.noreply.github.com>
2024-10-06 17:44:14 -0300

    Update Headers.h

--- HashCracker/Headers.h
+++ HashCracker/Headers.h
@@ -8,7 +8,13 @@
 #include <iomanip>
 #include <sstream>
 #include <chrono>
+#include <vector>
+#include <thread>
+#include <atomic>
 #include <openssl/sha.h>
 #include <openssl/md5.h>
+
+#include "HashingAlgorithms.h"
+#include "DeusNosAbandonou.h"
 
 #endif
