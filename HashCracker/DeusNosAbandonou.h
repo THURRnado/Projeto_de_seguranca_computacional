d1893b0424313271486e8563fe3370d95958a538
Kalil Teotonio <80278976+Galill@users.noreply.github.com>
2024-10-11 18:53:53 -0300

    Update DeusNosAbandonou.h

--- HashCracker/DeusNosAbandonou.h
+++ HashCracker/DeusNosAbandonou.h
@@ -5,7 +5,7 @@
 
 std::vector<std::string> DivideVector(std::vector<std::string> dictionary, int start_index, int end_index);
 
-std::string Tomie(std::vector<std::string>& sliced_dictionary, std::string hash, std::string hash_type, int thread_count);
+void Tomie(std::vector<std::string>& sliced_dictionary, std::string hash, std::string hash_type, int thread_count);
 
 std::string QuebraHash(std::vector<std::string> dictionary, std::string hash, std::string hash_type);
 
