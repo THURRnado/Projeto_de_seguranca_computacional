50dccc759f7d67f3043ba7204944d35d6d6d7d0a
Kalil Teotonio <80278976+Galill@users.noreply.github.com>
2024-10-11 21:21:38 -0300

    Update Main.cpp

--- HashCracker/Main.cpp
+++ HashCracker/Main.cpp
@@ -28,10 +28,13 @@ std::string QuebraHash(std::vector<std::string> dictionary, std::string hash, st
 
     if (hash_type.compare("MD5") == 0) {
         for (int i = 0; i < dictionary.size(); ++i) {
+            if (found)
+                break;
             password = dictionary[i];
             hashed_password = md5(password);
             if (hashed_password.compare(hash) == 0) {
                 std::cout << password << std::endl;
+                found = true;
                 break;
             }
         }
@@ -39,10 +42,13 @@ std::string QuebraHash(std::vector<std::string> dictionary, std::string hash, st
 
     if (hash_type.compare("SHA-1") == 0) {
         for (int i = 0; i < dictionary.size(); ++i) {
+            if (found)  
+                break;
             password = dictionary[i];
             hashed_password = sha1(password);
             if (hashed_password.compare(hash) == 0) {
                 std::cout << password << std::endl;
+                found = true;
                 break;
             }
         }
@@ -50,10 +56,13 @@ std::string QuebraHash(std::vector<std::string> dictionary, std::string hash, st
 
     if (hash_type.compare("SHA-256") == 0) {
         for (int i = 0; i < dictionary.size(); ++i) {
+            if (found)
+                break;
             password = dictionary[i];
             hashed_password = sha256(password);
             if (hashed_password.compare(hash) == 0) {
                 std::cout << password << std::endl;
+                found = true;
                 break;
             }
         }
@@ -61,10 +70,13 @@ std::string QuebraHash(std::vector<std::string> dictionary, std::string hash, st
 
     if (hash_type.compare("SHA-512") == 0) {
         for (int i = 0; i < dictionary.size(); ++i) {
+            if (found)
+                break;
             password = dictionary[i];
             hashed_password = sha512(password);
             if (hashed_password.compare(hash) == 0) {
                 std::cout << password << std::endl;
+                found = true;
                 break;
             }
         }
